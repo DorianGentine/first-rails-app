a00beb0bbad3ab8e6eac608d82ac53ae54dd2ddf
Dorian Gentine <dorian.gentine@mail.novancia.fr>
2020-11-26 16:30:24 +0100

    Add address to Restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_152149) do
+ActiveRecord::Schema.define(version: 2020_11_26_152849) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
   end
 
 end
